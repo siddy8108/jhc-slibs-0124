2d8b2ed01a6316651e561a87155a71485688379f
siddy8108 <155032506+siddy8108@users.noreply.github.com>
2024-01-04 21:00:10 -0500

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,5 +1,5 @@
 def call (tomcatIp,userName,credId,warName){
- sshagent([credID]){
+ sshagent([credId]){
                 sh 'scp -o StrictHostKeyChecking=no target/${warName} ${userName}@${tomcatIp}:/opt/tomcat9/webapps/'
                 sh 'ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/shutdown.sh'
                 sh 'ssh ${userName}@${tomcatIp} /opt/tomcat9/bin/startup.sh'
